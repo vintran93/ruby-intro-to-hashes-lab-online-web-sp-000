f71d0db39a6d0580e92d8aac3169357ea869bd10
Vincent Tran <vincentuong@yahoo.com>
2021-05-24 03:43:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,10 +17,11 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => 
     {
-      
+    :pieces => 4,
+    :names => {},
+    :rent_in_dollars=>{}
     }
   }
-  monopoly
 end
 
 def monopoly_with_third_tier
